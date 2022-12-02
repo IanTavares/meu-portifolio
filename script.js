8513013f1100c3b06b043244627063b85b6cbd80
IanTavares <icltavares@hotmail.com>
2022-12-01 21:11:26 -0300

    Ajeitei o menu

--- script.js
+++ script.js
@@ -4,3 +4,30 @@
     function abrirMenu() {
         menu.classList.toggle ('open')
     }
+    const el = document.querySelector('#name')
+    const text = "Ian Tavares"
+    const interval = 100
+    
+    function abrirMenu() {
+        menu.classList.toggle ('open')
+    }
+    
+    //Texto Digitado H1
+    
+    function showText (el, text, interval) {
+        const char = text.split("").reverse()
+    
+        const typer = setInterval(() => {
+    
+            if(!char.length) {
+                return clearInterval(typer)
+            }
+    
+            const next = char.pop()
+    
+            el.innerHTML += next
+    
+        }, interval)
+    }
+    
+    showText(el, text, interval)
